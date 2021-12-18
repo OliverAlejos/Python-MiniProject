80a6b2a89cbd05c245521f2051233727aea78759
OliverAlejos <90672319+OliverAlejos@users.noreply.github.com>
2021-12-18 03:50:45 -0500

    Update Gas.py

--- Gas.py
+++ Gas.py
@@ -13,23 +13,17 @@ with open ('PRICESBRUH.csv') as file:
     price = []
 
     for row in csv_file:
-   
+       # date_string = ("2015 {0}/")
         datetime_object = datetime.strptime('1/5/2015', "%m/%d/%Y")
         date_list.append(datetime_object)
         priceec = float(row[1])
         price.append(priceec)
-    
-
+  
 plt.plot(date_list, price, color = 'turquoise', linestyle = 'solid',
 		marker = 'o',label = "Gas Prices")
-
 
 plt.xlabel('DATE')
 plt.ylabel('PRICE (US DOLLAR)')
 plt.title('Gas Price Over Time (2015)', fontsize = 20)
 plt.grid()
-
-left = (1,5,2015)
-right = (12,18,2015)
- 
 plt.show()
